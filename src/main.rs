ddfb3551e63ce750f7fbfe1b9cc149fd0d465734
Daru <mica.tui@proton.me>
2024-06-11 17:31:48 +0200

    src: notify when color is picked

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use notify_rust::Notification;
 use std::process::Command;
 
 #[derive(Parser, Debug)]
@@ -12,8 +13,15 @@ struct Args {
 }
 fn main() {
     let args = Args::parse();
+    let notify = Notification::new()
+        .summary("Color Picker")
+        .body(("Copied {} to clipboard", color))
+        .show();
 
     Command::new("hyprpicker")
         .spawn()
         .expect("Command failed to execute");
+    if args.notify {
+        notify;
+    }
 }
