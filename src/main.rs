44b877ef3a53d967df98540305be68209d32c32b
Daru <mica.tui@proton.me>
2024-06-11 17:32:33 +0200

    src: copy to clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use notify_rust::Notification;
 use std::process::Command;
+use wl_clipboard_rs::copy::{MimeType, Options, Source};
 
 #[derive(Parser, Debug)]
 #[command(version,about,long_about = None)]
@@ -13,6 +14,11 @@ struct Args {
 }
 fn main() {
     let args = Args::parse();
+    let opts = Options::new();
+    let copy_to_clipboard = opts.copy(
+        Source::Bytes(color.to_string().into_bytes().into()),
+        MimeType::Autodetect,
+    );
     let notify = Notification::new()
         .summary("Color Picker")
         .body(("Copied {} to clipboard", color))
@@ -21,6 +27,10 @@ fn main() {
     Command::new("hyprpicker")
         .spawn()
         .expect("Command failed to execute");
+    if args.clipboard == true {
+        copy_to_clipboard;
+        println!("Color {} successfully copied to your clipboard!", color);
+    }
     if args.notify {
         notify;
     }
