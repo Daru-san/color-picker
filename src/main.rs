67b4745013dfd820368bc8c10920d4f0887daa48
Daru <mica.tui@proton.me>
2024-06-11 20:30:03 +0200

    src: simply command
    I realised that it would be better doing everything by default and
    expanding from there if need be
    Thus I would rather not use arguments and notify and copy to clipboard
    automatically

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,32 @@ use wl_clipboard_rs::copy::{MimeType, Options, Source};
 
 /// A simple color picker wrapper for hyprpicker
 #[derive(Parser, Debug)]
-#[command(version,about,long_about = None)]
-struct Args {
-    /// Notify the user when picking the color
-    #[arg(short, long)]
-    notify: bool,
-
-    /// Copy the color to the system clipboard
-    #[arg(short, long)]
-    clipboard: bool,
-}
+#[command(author,version,about,long_about = None)]
+struct Args {}
 
 fn main() {
-    let args = Args::parse();
     let proc = Command::new("hyprpicker")
         .stdout(Stdio::piped())
         .output()
         .unwrap();
+
     let color = String::from_utf8(proc.stdout).unwrap();
+
     let opts = Options::new();
+
     let copy_to_clipboard = opts.copy(
         Source::Bytes(color.to_string().into_bytes().into()),
         MimeType::Autodetect,
     );
+
+    let message = color + " has been copied to your clipboard";
+
     let notify = Notification::new()
         .summary("Color Picker")
-        .body(&color)
+        .body(&message)
         .show();
 
-    println!("{}", color);
-    if args.clipboard {
-        drop(copy_to_clipboard);
-        println!("Color {} successfully copied to your clipboard!", color);
-    }
-    if args.notify {
-        drop(notify);
-    }
+    println!("{}", message);
+    drop(copy_to_clipboard);
+    drop(notify);
 }
