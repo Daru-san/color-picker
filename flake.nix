4a138fb059045c379907b09ea2ce49f12166bed1
Daru <mica.tui@proton.me>
2024-07-03 09:16:27 +0200

    program: bump to 0.1.2

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       {
         packages.default = toolchain.buildRustPackage {
           pname = "color-picker";
-          version = "0.1.1";
+          version = "0.1.2";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = [pkgs.makeWrapper];
