f6d9087df1169baaf55b3849637d04d77d995e64
Daru <mica.tui@proton.me>
2024-08-09 23:05:52 +0200

    flake: formatting

--- flake.nix
+++ flake.nix
@@ -6,21 +6,23 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }: let
-    inherit (nixpkgs.lib) makeBinPath licenses maintainers;
-  in
-    utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }:
+    let
+      inherit (nixpkgs.lib) makeBinPath licenses maintainers;
+    in
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
         toolchain = pkgs.rustPlatform;
-      in rec
-      {
+      in
+      rec {
         packages = {
           default = packages.color-picker;
           color-picker = toolchain.buildRustPackage {
@@ -28,18 +30,16 @@
             version = "0.1.2";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
-            nativeBuildInputs = [pkgs.makeWrapper];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
             postInstall = ''
               wrapProgram $out/bin/color-picker \
-                --prefix PATH : ${
-                makeBinPath [pkgs.hyprpicker]
-              }
+                --prefix PATH : ${makeBinPath [ pkgs.hyprpicker ]}
             '';
             meta = {
               description = "A simple wrapper program for hyprpicker with notifications";
               homepage = "https://github.com/Daru-san/color-picker";
               license = licenses.mit;
-              maintainers = [maintainers.daru-san];
+              maintainers = [ maintainers.daru-san ];
               mainProgram = "color-picker";
             };
           };
@@ -47,13 +47,11 @@
         formatter = pkgs.nixfmt-rfc-style;
         apps = {
           default = apps.color-picker;
-          color-picker = utils.lib.mkApp {drv = packages.color-picker;};
+          color-picker = utils.lib.mkApp { drv = packages.color-picker; };
         };
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            (with toolchain; [
-              rustLibSrc
-            ])
+            (with toolchain; [ rustLibSrc ])
             (with pkgs; [
               rustc
               cargo
